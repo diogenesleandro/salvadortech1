27748ba555d57d181321a8ae203d20e92d118a93
diogenesleandro <diogenes.pereira@cps.sp.gov.br>
2026-05-11 18:48:53 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $n1 = "Hello";
+        $n2 = "World";
+        echo "$n1 $n2";
+
+    ?>
+</body>
+</html>
